f3bf6f5b47087288cf41b7a9db06b686c8edd443
Sidver-pod <75096302+Sidver-pod@users.noreply.github.com>
2022-01-20 13:01:48 +0530

    Code clean up, importing custom file
    + Cleaned the code by moving many lines of code over to the "routes.js" file
    + Imported "routes.js" into current file

--- node-server.js
+++ node-server.js
@@ -1,91 +1,9 @@
 //importing 'http' core module
 const http = require('http');
 
-//importing 'fs' core module
-const fs = require('fs');
+//importing custom JS file that contains routing information
+const routes = require('./routes');
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-
-    //routing requests
-    if(url === '/home') {
-        res.write('<html>');
-        res.write('<head><title>Home</title></head>');
-        res.write('<body><h1>Welcome Home</h1></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    else if(url === '/about') {
-        res.write('<html>');
-        res.write('<head><title>About</title></head>');
-        res.write('<body><h1>Welcome to the About page</h1></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    else if(url === '/node') {
-        res.write('<html>');
-        res.write('<head><title>NodeJS</title></head>');
-        res.write('<body><h1>Welcome to my Node.js project!</h1></body>');
-        res.write('</html>');
-        return res.end();
-    }
-    else if(url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Form</title></head>');
-        res.write('<body>');
-        
-        //reading text from the file to print above the "Form"
-        if(fs.existsSync('message.txt')) {
-            fs.readFile('message.txt', 'utf-8', (err, data) => {
-                console.log(data);
-                res.write(`<h3>${data}</h3>`);
-                res.write('<form action="/message" method="POST"><input type="text" name="input_message" placeholder="enter message"><input type="submit" innerText="Message" value="message" name="message"></form>');
-                res.write('</body>');
-                res.write('</html>');
-                return res.end();
-            });
-        }
-        else {
-            res.write('<form action="/message" method="POST"><input type="text" name="input_message" placeholder="enter message"><input type="submit" innerText="Message" value="message" name="message"></form>');
-            res.write('</body>');
-            res.write('</html>');
-            return res.end();
-        }
-    }
-
-    //redirecting requests
-    if(url === '/message' && method === 'POST') {
-        const body = []; //to store chunk of the request data
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            // console.log(parsedBody);
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/'); //after writing the message to file it goes back to '/'
-                return res.end();
-            });
-        });
-        //fs.writeFileSync('message.txt', 'DUMMY');
-        //res.writeHead(302, {/* headers come here */}) // (or) follow the two step way below (now above):-
-        /* 
-            NOTE - writeFileSync has been changed to writeFile so that the user's page will ridirect even before writing to the file is over!
-        */
-    }
-
-    // console.log(`Siddharth Verma`);
-    // console.log(req.url, req.method, req.headers);
-    // res.write('<html>');
-    // res.write('<head><title>My First Page</title></head>');
-    // res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    // res.write('</html>');
-    // res.end();
-    // //process.exit();
-});
+const server = http.createServer(routes);
 
 server.listen('4000');
