74ef34301deb31b5350c90cc9ffe87bac76395f3
Sidver-pod <75096302+Sidver-pod@users.noreply.github.com>
2022-01-26 01:36:02 +0530

    Express JS, middleware, app.use(), app.listen()
    + Express JS
    + "npm install --save express"
    + app.use((req, res, next) => { }) - Middleware
    + app.listen() - Shorter and easier to code

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 //importing 'http' core module
 const http = require('http');
 
-//importing custom JS file that contains routing information
-const routes = require('./routes');
+//importing 'express'
+const express = require('express');
 
-const server = http.createServer(routes);
+const app = express();
 
-server.listen('4000');
+//middleware
+app.use((req, res, next) => {
+    console.log('In the middleware');
+    next(); //allows the request to continue to the next middleware
+});
+
+//middleware
+app.use((req, res, next) => {
+    console.log('In another middleware');
+    res.send(`<h1>Hello from Express!</h1>`);
+});
+
+/*
+    const server = http.createServer(app);
+    server.listen('4000');
+*/
+
+app.listen(4000);
