2834f1f503b775cec776c307e1fb78dca08ee3b5
Sidver-pod <75096302+Sidver-pod@users.noreply.github.com>
2022-01-27 22:02:10 +0530

    Code clean up, imported custom files, filtered paths, status code 404
    + Code clean up - moved routing codes into a separate folder by the name of "routes"
    + Imported custom files
    + Filtered paths starting with the same pathnames
    + "Page Not Found!" page was added if the user ever surfed into an unrecognized page and status code 404 is returned
    + Two other files: admin.js, shop.js

--- app.js
+++ app.js
@@ -6,19 +6,20 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+//importing custom files
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended: false})); //helps parse the body of request
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title" placeholder="Title"><input type="text" name="size" placeholder="Size"><button type="submit">Add Product</button></form>');
-});
+//(order of Middleware matters)
+app.use('/admin', adminRoutes); //custom imported file is a middleware! here only URLs starting with '/admin' will enter in; this helps in filtering URLs having the same paths together;
+app.use('/shop', shopRoutes);
 
-app.use('/product', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/'); //redirects user to given URL
-});
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
+//when the user surfs into an unrecognized page; the URL by default is '/'
+//the process of chaining is used below; here 'send' occurs in the end after 'status' has been defined!
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page Not Found!</h1>');
 });
 
 app.listen(4000);
