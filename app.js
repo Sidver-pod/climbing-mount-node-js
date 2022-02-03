e91610d13c6483b418283d8031a23a849d19324d
Sidver-pod <75096302+Sidver-pod@users.noreply.github.com>
2022-02-03 17:01:17 +0530

    'path' module, static CSS files
    + Imported 'path' module
    + Imported custom files: /contactus, /success
    + express.static() => Serving CSS files statically (i.e. directly forwarding the file to the file system and not to express or any middleware!)
    + res.status().sendFile()

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+//importing 'path' module
+const path = require('path');
+
 //importing 'express'
 const express = require('express');
 
@@ -9,17 +12,22 @@ const app = express();
 //importing custom files
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const contactUsRoutes = require('./routes/contactus');
+const successRoutes = require('./routes/success');
 
 app.use(bodyParser.urlencoded({extended: false})); //helps parse the body of request
+app.use(express.static(path.join(__dirname, 'public'))); //serving files statically! (CSS files)
 
 //(order of Middleware matters)
 app.use('/admin', adminRoutes); //custom imported file is a middleware! here only URLs starting with '/admin' will enter in; this helps in filtering URLs having the same paths together;
 app.use('/shop', shopRoutes);
+app.use('/contactus', contactUsRoutes);
+app.use('/success', successRoutes);
 
-//when the user surfs into an unrecognized page; the URL by default is '/'
+//when the user surfs into an unrecognized page; (the URL by default is '/')
 //the process of chaining is used below; here 'send' occurs in the end after 'status' has been defined!
-app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found!</h1>');
+app.use('/', (req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(4000);
