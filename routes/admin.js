a54833b6b92ad60ae7e340607eeefa4b04090ff6
Sidver-pod <75096302+Sidver-pod@users.noreply.github.com>
2022-02-03 16:48:37 +0530

    /admin/add-product => GET, POST
    + Imported path
    + Imported express
    + express.Router()
    + Imported 'path.js' from util folder
    + res.sendFile()
    + res.redirect()
    + Exporting router

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,16 @@
+const path = require('path');
+
 const express = require('express');
 
 const router = express.Router();
 
+//importing a path utility to make navigating among files a little cleaner!
+const rootDir = require('../util/path');
+
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title" placeholder="Title"><input type="text" name="size" placeholder="Size"><button type="submit">Add Product</button></form>');
+    // (__dirname, '../', 'views', 'add-product.html')
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 // /admin/add-product => POST
