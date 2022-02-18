79d4f0728b5af3fb71e9107f96ccc7386d1eaeb0
Sidver-pod <75096302+Sidver-pod@users.noreply.github.com>
2022-02-18 17:46:33 +0530

    Merged multiple route files into one
    + shop
    + cart
    + contact us
    + success

--- routes/shop.js
+++ routes/shop.js
@@ -3,8 +3,16 @@ const express = require('express');
 const router = express.Router();
 
 const productsController = require('../controllers/products');
+const successController = require('../controllers/miscellaneous');
 
-// /shop => GET
-router.get('/', productsController.getShop);
+router.get('/shop', productsController.getShop);
+
+router.get('/cart', productsController.getCart);
+
+router.get('/contactus', successController.getContactUs);
+
+router.post('/contactus', successController.postContactUs);
+
+router.get('/success', successController.getSuccess);
 
 module.exports = router;
