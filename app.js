1745ce22f7f1542b20c5f0b2d27c7ac2d2b4de6e
Sidver-pod <75096302+Sidver-pod@users.noreply.github.com>
2022-03-06 11:55:52 +0530

    Updated with Sequelize package
    + Imported "database.js" (or 'sequelize' object)
    + sequelize.sync() - syncs all defined "models" to the database (meaning: makes tables out of models; if tables exist then it makes relations among them)

--- app.js
+++ app.js
@@ -1,29 +1,30 @@
-//importing 'path' module
 const path = require('path');
 
-//importing 'express'
 const express = require('express');
-
-//importing 'body-parser'
 const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const sequelize = require('./util/database');
 
 const app = express();
 
-//importing custom files
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false})); //helps parse the body of request
-app.use(express.static(path.join(__dirname, 'public'))); //serving files statically! (CSS files)
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//(order of Middleware matters)
-app.use('/admin', adminRoutes); //custom imported file is a middleware! here only URLs starting with '/admin' will enter in; this helps in filtering URLs having the same paths together;
-app.use('/', shopRoutes);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-//when the user surfs into an unrecognized page; (the URL by default is '/')
-//the process of chaining is used below; here 'send' occurs in the end after 'status' has been defined!
-app.use('/', (req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
+app.use(errorController.get404);
 
-app.listen(4000);
+//syncs all defined "models" to the database (meaning: makes tables out of models; if tables exist then it makes relations among them)
+sequelize.sync()
+ .then(result => console.log('database sync: CHECK'))
+ .catch(err => console.log(err));
+
+app.listen(3000);
