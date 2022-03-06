615b6897f1a6aea6aa0613a19b6b5b3ac54a846e
Sidver-pod <75096302+Sidver-pod@users.noreply.github.com>
2022-03-06 11:44:37 +0530

    Updated with Sequelize package
    + Imported "database.js" (or the 'sequelize' object)
    + Created 'product' model
    + Sequelize.define()

--- models/product.js
+++ models/product.js
@@ -1,44 +1,28 @@
-const db = require('../util/database');
+const Sequelize = require('sequelize');
 
-module.exports = class Product {
-  constructor(id, title, imageUrl, description, price) {
-    this.id = id; //'null' should be passed for a new product; when '/admin/edit-product' then pass in 'id'
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.description = description;
-    this.price = price;
-  }
+const sequelize = require('../util/database');
 
-  save() {
-    //updating a product
-    if(this.id != null) {
-      return db.execute(
-        'UPDATE products SET title = ?, price = ?, description = ?, imageUrl = ? WHERE products.id = ?',
-        [this.title, this.price, this.description, this.imageUrl, this.id]
-      );
-    }
-    //inserting a new product
-    else {
-      return db.execute(
-        'INSERT INTO products (title, price, description, imageUrl) VALUES(?, ?, ?, ?)',
-        [this.title, this.price, this.description, this.imageUrl]
-      );
-    }
+//product model
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
+});
 
-  static fetchAll() {
-    return db.execute('SELECT * FROM products');
-  }
-
-  static findById(id) {
-    return db.execute(
-      'SELECT * FROM products WHERE products.id = ?', [id]
-    );
-  }
-
-  static deleteById(id) {
-    return db.execute(
-      'DELETE FROM products WHERE products.id = ?', [id]
-    );
-  }
-};
+module.exports = Product;
