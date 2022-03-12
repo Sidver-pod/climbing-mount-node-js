7dfdaf066da75de82ca3d4d5e1e54d694fed7578
Sidver-pod <75096302+Sidver-pod@users.noreply.github.com>
2022-03-12 21:11:23 +0530

    Updated with Sequelize package
    + Previous code now works in the background (Sequelize takes care of that)
    + Sequelize is used to minimise SQL queries and to focus on Node.JS
    + 'Sequelize' is a class (or constructor) hence the way 'sequelize' is defined

--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 //database, username, password
-const sequelize = new Sequelize('node-complete', 'root', 'frozenAirM1*', {
+const sequelize = new Sequelize('node-complete', 'root', 'node_Complete*', {
     dialect: 'mysql',
     host: 'localhost'
 });
