f5648e0aa9a48cf3be8d42e539af84fe013ec53b
Sidver-pod <75096302+Sidver-pod@users.noreply.github.com>
2022-03-18 00:16:12 +0530

    Database models, made a new field for req, defined associations
    + Database models
    + Made a new 'user' field for the 'req' object
    + Defined associations among the tables
          - User, Product (One-Many)
          - User, Cart (One-One)
          - Cart, Product (Many-Many)

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
 
+/* database models */
+const Product = require('./models/product');
+const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,14 +23,46 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    User.findByPk(1)
+     .then(user => {
+         /* making a new 'user' field for the request object */
+         req.user = user;
+         next();
+     })
+     .catch();
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+/* defining relationship among tables */
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, {through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
 //syncs all defined "models" to the database (meaning: makes tables out of models; if tables exist then it makes relations among them)
 sequelize.sync()
- .then(result => console.log('database sync: CHECK'))
+ .then(result => {
+    console.log('database sync: CHECK');
+    return User.findByPk(1);
+ })
+ .then(user => {
+     if(!user) {
+         return User.create({username: 'Sid', email: 'sid@borders.com'});
+     }
+     return user;
+ })
+ .then(user => {
+    //  console.log(user);
+    return user.createCart();
+ })
+ .then(cart => {
+    app.listen(3000);
+ })
  .catch(err => console.log(err));
-
-app.listen(3000);
